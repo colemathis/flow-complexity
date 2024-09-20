b44bd85a2a09da2f0b99fc7706f8cd28548e90f7
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2024-09-20 14:08:38 -0700

    create workflow scripts

---
+++ scripts/MS00_MWE/launch.jl
@@ -0,0 +1,72 @@
+"""
+##########################################################################
+
+launch.jl
+
+Usage:
+    launch.jl <CSV file> <sim>
+
+where:
+    <CSV file>  : path of the CSV file relative to data/
+    <sim>       : simulation number
+
+##########################################################################
+"""
+
+"""
+##########################################################################
+IMPORTS
+##########################################################################
+"""
+
+using DrWatson
+@quickactivate :FlowComplexity
+
+using DataFrames
+
+"""
+##########################################################################
+FUNCTIONS
+##########################################################################
+"""
+
+function print_usage()
+    println("""
+    Usage:
+        launch.jl <CSV file> <sim>
+
+    where:
+        <CSV file>  : path of the CSV file relative to data/
+        <sim>       : simulation number
+    """)
+end
+
+"""
+##########################################################################
+MAIN
+##########################################################################
+"""
+
+# Check the number of command line arguments
+if length(ARGS) < 2
+    print_usage()
+    exit(1)
+end
+
+# load the parameters from the CSV, e.g. "sims/MS00_MWE/array1.csv"
+array_fn = ARGS[1]
+println("Loading parameter file $array_fn.")
+params_df = FlowComplexity.GetParamsFromCSV(datadir(array_fn))
+
+# get a param dictionary for simulation number <sim>
+sim = parse(Int, ARGS[2])
+println("Reading parameters for simulation number $sim.")
+params_dict = FlowComplexity.DF_row_to_dict(params_df, sim)
+
+# create simulation object
+println("Creating simulation object.")
+s = FlowComplexity.Simulation(; params_dict...)
+
+# launch it
+println("Launching simulation.")
+FlowComplexity.RunSimulation(s)
---
+++ scripts/MS00_MWE/queue.jl
@@ -0,0 +1,36 @@
+using DrWatson
+@quickactivate :FlowComplexity
+
+using DataFrames
+
+# define a configuration template
+params_df = DataFrame(
+    mass = 1000,
+    graph_type = "line",
+    N_reactors = 1,
+    forward_rate = 1.0e-2,
+    outflow_rate = 5.0,
+    total_time = 1.0,
+    sim_number = 1,
+    save_time_series = true,
+    save_parameters = true,
+    save_graph = false,
+    save_simulation = true,
+    save_directory = "MS00_MWE",
+    notes = "dummy task"
+)
+
+# dummy example: copy the template 10 times and change the sim number
+first_row = params_df[1, :]
+new_rows = [first_row for _ in 1:10]
+params_df = DataFrame(new_rows)
+
+for i in 1:10
+    params_df[i, :sim_number] = i
+end
+
+println(params_df)
+
+arr_fn = "sims/MS00_MWE/array.csv"
+save(datadir(array_fn), params_df) ;
+
