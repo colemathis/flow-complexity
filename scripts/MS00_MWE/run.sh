e7550135723c16cf2c69d5a8b461b1c2108602b2
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2024-09-20 15:08:48 -0700

    finish writing slurm script

--- scripts/MS00_MWE/run.sh
+++ scripts/MS00_MWE/run.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=test-array
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1-1000
+#SBATCH --array=1-100
 #SBATCH -e /dev/null
 #SBATCH -o /dev/null
 
 module load julia
-
-echo "This is array task ${SLURM_ARRAY_TASK_ID}" >> test-array.sh.log
-
-julia test-array.jl ${SLURM_ARRAY_TASK_ID} >> test-array.jl.log
+ID=${SLURM_ARRAY_TASK_ID}
+mkdir -p ../../data/sims/MS00_MWE/logs
+julia launch.jl sims/MS00_MWE/array.csv ${ID} >> ../../data/sims/MS00_MWE/logs/launch.jl.${ID}.log
