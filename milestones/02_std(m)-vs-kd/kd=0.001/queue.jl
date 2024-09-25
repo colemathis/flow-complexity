5497ec64977a041111267f6815028a6d52233ccf
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2024-09-25 15:19:55 -0700

    update script

---
+++ milestones/02_std(m)-vs-kd/kd=0.001/queue.jl
@@ -0,0 +1,44 @@
+using DrWatson
+@quickactivate :FlowComplexity
+
+using DataFrames
+
+# Define directories and paths
+current_dir = pwd()
+milestones_dir = joinpath(projectdir(), "milestones")
+relative_path = relpath(current_dir, milestones_dir)
+
+# Define a configuration template
+params_template = DataFrame(
+    mass = 1000,
+    graph_type = "line",
+    N_reactors = 9,
+    forward_rate = 0.001,
+    outflow_rate = 0.0,
+    total_time = 100,
+    sim_number = 1,
+    save_time_series = true,
+    save_parameters = true,
+    save_graph = true,
+    save_simulation = true,
+    save_directory = relative_path,
+    notes = "none"
+)
+
+# Number of simulations
+n = 100
+
+# Generate DataFrame with n rows based on the template
+params_df = vcat([params_template for _ in 1:n]...)
+
+# Update the sim_number for each row
+params_df.sim_number .= 1:n
+
+# Customize parameters
+for i in 1:100
+    params_df[i, :outflow_rate] = i*0.001
+end
+
+# Save the DataFrame to a CSV file
+array_fn = "./data/array.csv"
+save(array_fn, params_df)
