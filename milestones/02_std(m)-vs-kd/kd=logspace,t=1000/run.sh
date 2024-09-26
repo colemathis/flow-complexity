fa641bc964f796b0b4f7630b43c543706bbbc756
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2024-09-25 17:17:39 -0700

    add experiment

---
+++ milestones/02_std(m)-vs-kd/kd=logspace,t=1000/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH --job-name=flow-complexity
+#SBATCH --partition=htc
+#SBATCH --qos=public
+#SBATCH --array=1-100
+#SBATCH --time=04:00:00
+#SBATCH --output=./data/logs/slurm_%a.log
+#SBATCH --error=./data/logs/slurm_%a.log
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user="%u@asu.edu"
+
+# Load necessary modules
+module load julia
+
+# Create logs directory if it doesn't exist
+mkdir -p ./data/logs
+
+# Get the task ID from the SLURM array
+ID=${SLURM_ARRAY_TASK_ID}
+
+# Run the Julia script with the task ID and redirect output to a log file
+srun julia launch.jl ./data/array.csv ${ID} >> ./data/logs/launch.jl_${ID}.log
