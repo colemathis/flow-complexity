4367f14560c850c324079ba274dcb1fa5b4b0e7c
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2024-10-13 14:13:54 -0700

    update script

--- milestones/04_mu(m)-vs-kd/A_kdmax-1e2/run_sol.sh
+++ milestones/04_mu(m)-vs-kd/A_kdmax-1e2/run_sol.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --job-name=flow-complexity
-#SBATCH --partition=htc
+#SBATCH --partition=general
 #SBATCH --qos=public
 #SBATCH --array=1-100
-#SBATCH --time=04:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --output=./data/logs/slurm_%a.log
 #SBATCH --error=./data/logs/slurm_%a.log
 #SBATCH --mail-type=NONE
