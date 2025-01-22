ae01ce44b6a05052e37671b18bc35b3549c46289
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2025-01-22 11:33:57 -0500

    move things

---
+++ milestones/08_tauleap_distance-from-source/B_dt=1e-2/params.jl
@@ -0,0 +1,27 @@
+nsim = 100
+params_template = Dict(
+    :mass           => 1000,
+    :inflow_mols    => 0,
+    :graph_type     => "lattice-2way",
+    :N_reactors     => 25,
+    :forward_rate   => 1e-3,
+    :outflow_rate   => 0.0,
+    :total_time     => 100,
+    :output_time    => 1.0,
+    :N_inflow       => 1,
+    :sim_number     => 1,
+    :method         => "tau-leaping",
+    :dt             => 1e-2,
+    :random_seed    => 1,
+)
+
+params_array = [copy(params_template) for _ in 1:nsim]
+
+vals = exp10.(LinRange(-6,1,nsim))
+# vals = LinRange(1, 100, 10)
+# vals = round.(Int, vals)*10
+
+for i in 1:nsim
+    params_array[i][:sim_number] = i
+    params_array[i][:outflow_rate] = vals[i]
+end
