894079feec0128ef64453589222ae4c1d4e93bd3
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2025-05-05 13:55:45 -0700

    add experiment

---
+++ milestones/23_distance-from-source-outflow-fixed/B_tmax=1e3/params.jl
@@ -0,0 +1,27 @@
+nrepeat = 1
+
+nsim = 100
+diffusion_rates = exp10.(LinRange(-6,1,nsim))
+
+params_template = Dict(
+    # simulation parameters
+    :save_interval    	=> 10,
+    :method         	=> "tau-leaping",
+    :dt             	=> 1e-3,
+    :random_seed    	=> "random",
+
+    # physical parameters
+    :total_time     	=> 1000,
+    :initial_mass       => 0,
+
+    # topological parameters
+    :graph_type         => "lattice-2way",
+    :randomize_edges    => false,
+    :N_reactors     	=> 25,
+
+    # reaction rates
+    :inflow_mols        => 1e3,
+    :forward_rate   	=> 1e-3,
+    :diffusion_rate   	=> diffusion_rates,
+    :outflow_rate       => "equal-to-diffusion-rate",
+)
