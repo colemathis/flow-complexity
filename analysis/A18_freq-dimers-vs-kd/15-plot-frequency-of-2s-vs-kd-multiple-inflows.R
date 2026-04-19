3b2a6a897412f00ccb890627bd0886ae89e0553e
Alexandre Champagne-Ruel <alexandre.champagne-ruel@umontreal.ca>
2026-04-19 15:43:53 -0700

    add new analysis for revision

---
+++ analysis/A18_freq-dimers-vs-kd/15-plot-frequency-of-2s-vs-kd-multiple-inflows.R
@@ -0,0 +1,211 @@
+################################################################################
+# IMPORTS
+################################################################################
+
+options(conflicts.policy = list(warn.conflicts = FALSE))
+
+library(tidyverse)   # includes dplyr, ggplot2, etc.
+library(latex2exp)
+
+################################################################################
+# PARAMETERS
+################################################################################
+
+ID         				<<- "15-plot-frequency-of-2s-vs-kd-multiple-inflows"
+USE_CACHE  				<<- TRUE
+PRINT_FIGS 				<<- FALSE
+SAVE_FIGS 			 	<<- TRUE
+
+DATA_DIR      			<<- "../../datasets/D05_inflow=1e3_1e4_kd=1e-4_1e1/data"
+CACHE_DIR     			<<- file.path("cache", ID)
+FIGS_DIR      			<<- "figs"
+
+PARAMS_FILE 			<<- "params.csv"
+TIMESERIES_FILES		<<- "timeseries.csv"
+CACHE_FILE       		<<- paste0(ID, ".csv")
+FIGS_FILE        		<<- paste0(ID, ".pdf")
+
+CACHE_PATH   			<<- file.path(CACHE_DIR, CACHE_FILE)
+PARAMS_PATH  			<<- file.path(DATA_DIR, PARAMS_FILE)
+
+################################################################################
+# FUNCTIONS
+################################################################################
+
+setwd_to_script_path <- function() {
+
+	# Case 1: Rscript
+	args <- commandArgs(trailingOnly = FALSE)
+	file_arg <- grep("^--file=", args, value = TRUE)
+	if (length(file_arg) > 0) {
+		fn = normalizePath(sub("^--file=", "", file_arg))
+		setwd(dirname(fn))
+		return(invisible())
+	}
+
+	# Case 2: VSCode
+	if (!is.null(sys.frames()[[1]]$ofile)) {
+		fn = normalizePath(sys.frames()[[1]]$ofile)
+		setwd(dirname(fn))
+		return(invisible())
+	}
+
+	# Error
+	stop("Cannot determine script path.")
+
+}
+
+#==============================================================================#
+
+load_params <- function() {
+
+	read_csv(PARAMS_PATH, show_col_types = FALSE)
+
+}
+
+#==============================================================================#
+
+load_and_process_time_series <- function() {
+
+	timeseries_files <- list.files(DATA_DIR, pattern = TIMESERIES_FILES, recursive = TRUE, full.names = TRUE)
+
+	ts_all <- map_dfr(timeseries_files, function(file) {
+		if (file.info(file)$size == 0) return(NULL)
+		ts <- read_csv(file, show_col_types = FALSE, progress = FALSE)
+		process_data(ts)
+	}, .progress = TRUE)
+
+	# Save the processed data to a cache file
+	dir.create(dirname(CACHE_PATH), recursive = TRUE, showWarnings = FALSE)
+	write_csv(ts_all, CACHE_PATH)
+
+	return(ts_all)
+
+}
+
+#==============================================================================#
+
+process_data <- function(ts) {
+
+	MAX_TIME   <- params$total_time[1]
+	N_REACTORS <- params$N_reactors[1]
+
+	ts <- ts %>%
+		filter(time == MAX_TIME) %>%
+		filter(integer == 2)
+
+	ts <- ts %>%
+		left_join(params %>% select(sim_number, diffusion_rate), by = "sim_number") %>%
+		left_join(params %>% select(sim_number, inflow_mols), by = "sim_number")
+
+	ts %>%
+		group_by(diffusion_rate, inflow_mols, integer) %>%
+		summarize(
+			mean_frequency = sum(frequency, na.rm = TRUE) / N_REACTORS,
+			sd_frequency = sqrt(sum((frequency - mean_frequency)^2) / (N_REACTORS - 1)),
+			.groups = "drop"
+		)
+
+}
+
+#==============================================================================#
+
+load_cached_data <- function() {
+
+	read_csv(CACHE_PATH, show_col_types = FALSE)
+
+}
+
+#==============================================================================#
+
+plot_figure <- function(ts) {
+
+	p <- ggplot(ts,
+				aes(
+					x = diffusion_rate, 
+					y = mean_frequency, 
+					color = factor(log10(inflow_mols))
+				))
+
+	p <- p + geom_point(size = 0.5, alpha = 0.25)
+	# p <- p + geom_smooth(method = "loess", span = 0.5, se = FALSE, size = 0.5)
+	p <- p + geom_line(stat = "smooth", method = "loess", span = 0.5, se = FALSE, size = 0.75, alpha = 0.75)
+
+	p <- p + scale_x_log10(
+		labels = scales::trans_format("log10", function(x) TeX(sprintf("$10^{%d}$", x)))
+	)
+	# p <- p + scale_color_manual(values = c("darkblue", "blue", "yellow", "red"))
+
+	p <- p + labs(
+		x = TeX("Diffusion coefficient $k_d$"),
+		y = "Frequency of Dimers",
+		color = TeX("$\\log(I)$")
+		# title = "Populations of 2’s, sweep over diffusion with multiple inflows",
+		# caption = ID
+	)
+
+	p <- p + theme_minimal(base_size = 11) # Set base font size for the theme
+
+	p <- p + theme(
+		legend.justification=c(1,1), 
+		legend.position=c(0.95,0.95),
+		legend.background = element_rect(fill = "white", color = "black"), # Optional: Customize legend background
+		legend.key.size = unit(0.15, "cm"),    # Decrease key size
+		# legend.text = element_text(size = 6), # Decrease text size
+		# legend.title = element_text(size = 6), # Decrease title size
+		plot.title = element_text(size = 6),
+		plot.caption = element_text(size = 6, color = "grey50")
+	)
+
+	p <- p + theme(panel.border = element_rect(color = "black", fill = NA, size = 0.5))
+
+	library(scales)
+
+	p <- p + scale_color_viridis_d(
+	option = "viridis",   # "magma" or "plasma" give a dark-to-bright progression
+	begin = 0.1,        # start slightly lighter than pure black
+	end = 0.7,          # stop before pure white
+	direction = 1,      # 1 = dark→light
+	labels = function(x) sprintf("%.1f", as.numeric(x))
+	)
+
+	# # show only one decimal in the legend labels
+	# p <- p + scale_color_discrete(labels = function(x) {
+	# 	x <- as.numeric(x)
+	# 	x <- round(x, 1)
+	# 	return(x)
+	# })
+
+	height	<- 60
+	width	<- 80
+
+	if (PRINT_FIGS) {
+		options(vsc.dev.args = list(width = width, height = height, res=300, units = "mm"))
+		print(p)
+	}
+
+	if (SAVE_FIGS) {
+		out_file <- file.path(FIGS_DIR, FIGS_FILE)
+		ggsave(filename = out_file, plot = p, width = width, height = height, units = "mm", create.dir = TRUE)
+	}
+
+}
+
+################################################################################
+# MAIN SCRIPT
+################################################################################
+
+# Set-up environment
+setwd_to_script_path()
+
+# Load and process data
+params <- load_params()
+
+if (file.exists(CACHE_PATH) && USE_CACHE) {
+	data   <- load_cached_data()
+} else {
+	data   <- load_and_process_time_series()
+}
+
+# Plot the figure
+p <- plot_figure(data)
